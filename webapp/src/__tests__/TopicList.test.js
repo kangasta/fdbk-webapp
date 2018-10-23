5f32a11e9813e2a61383d6d1846d3746a3f6fdcf
Toni Kangas <toni.ar.kangas@gmail.com>
2018-10-24 01:05:12 +0300

    Try moving await

--- webapp/src/__tests__/TopicList.test.js
+++ webapp/src/__tests__/TopicList.test.js
@@ -5,8 +5,7 @@ import TopicList from '../TopicList';
 
 describe('TopicList', () => {
 	it('shows loading message while waiting fetch result', async () => {
-		const wrapper = mount(<TopicList/>);
+		const wrapper = await mount(<TopicList/>);
 		expect(wrapper.find('.TopicList').hasClass('Loading')).toBe(true);
-		await wrapper.instance().componentDidMount();
 	});
 });
