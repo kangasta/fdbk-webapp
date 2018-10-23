c7c2bdaa48bdb1590c613b1c9ee15bdebdacf549
Toni Kangas <toni.ar.kangas@gmail.com>
2018-10-24 01:31:50 +0300

    Use shallow in App test

--- webapp/src/__tests__/TopicList.test.js
+++ webapp/src/__tests__/TopicList.test.js
@@ -5,7 +5,8 @@ import TopicList from '../TopicList';
 
 describe('TopicList', () => {
 	it('shows loading message while waiting fetch result', async () => {
-		const wrapper = await mount(<TopicList/>);
+		const wrapper = mount(<TopicList/>);
 		expect(wrapper.find('.TopicList').hasClass('Loading')).toBe(true);
+		await wrapper.instance().componentDidMount();
 	});
 });
