58c3f3200e3ccd4216e7528551aa2b68cf642ba6
Toni Kangas <toni.ar.kangas@gmail.com>
2019-02-15 22:07:00 +0200

    Make summary autoupdate interval configurable

--- webapp/src/__tests__/Summary.test.js
+++ webapp/src/__tests__/Summary.test.js
@@ -22,4 +22,18 @@ describe('Summary',() => {
 			expect(wrapper.find('.Summary').hasClass('Error')).toBe(true);
 		});
 	});
+	it('automatically updates on specified interval', () => {
+		const fetch_promise = new Promise((resolve) => setTimeout(() => { resolve({json: () => ({topic: 'topic'})}); }), 1000);
+		global.fetch = jest.fn(() => fetch_promise);
+
+		const wrapper = shallow(<Summary topic_id='topic' update_interval={1000}/>);
+		expect(wrapper.find('.Summary').hasClass('Loading')).toBe(true);
+
+		jest.runOnlyPendingTimers();
+		const n_calls = global.fetch.mock.calls.length;
+		[1, 2, 3, 4].map(i => i + n_calls).forEach(i => {
+			jest.runTimersToTime(1000);
+			expect(global.fetch).toHaveBeenCalledTimes(i);
+		});
+	});
 });
